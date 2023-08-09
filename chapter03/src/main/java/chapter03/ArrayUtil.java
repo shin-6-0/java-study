9e0ddb4979886cd61dc5224a0f5e61823c7985e2
shin-6-0 <syk6715@naver.com>
2023-08-09 16:54:08 +0900

    static

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -3,32 +3,26 @@ package chapter03;
 import java.util.Arrays;
 
 public class ArrayUtil {
-	public static void main(String[] args) {
-		int[]a = {10,20,30,40};
-		
-		double[]d = ArrayUtil.intToDoublie(a);
-		System.out.println(Arrays.toString(d));
+
+	public static double[] intToDouble(int[] a) {
+		double[] d = new double[a.length];
+		for(int i = 0; i < a.length; i++) {
+			d[i] = a[i];
+		}
+
+		return d;
 	}
 
-	private static double[] intToDoublie(int[] a) {
-		// TODO Auto-generated method stub
-		double[] d = new double[a.length];
-		for(int i=0;i<a.length;i++) {
-			d[i]=a[i];
-		}
-		return null;
-	}
-	
 	public static int[] doubleToInt(double[] d) {
 		int[] a = new int[d.length];
-		for(int i=0;i<d.length;i++) {
-			
+		for(int i = 0; i < d.length; i++) {
+			a[i] = (int)d[i];
 		}
 		return a;
 	}
 
-	public static double[] intToDouble(int[] is) {
-		// TODO Auto-generated method stub
+	public static int[] concat(int[] a1, int[] a2) {
 		return null;
 	}
+
 }
