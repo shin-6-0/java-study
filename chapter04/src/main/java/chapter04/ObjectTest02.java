f8635d851f3c2ad8b278393dd18c6e4865bfbd10
shin-6-0 <syk6715@naver.com>
2023-08-11 11:06:13 +0900

    hashCode, equals

---
+++ chapter04/src/main/java/chapter04/ObjectTest02.java
@@ -0,0 +1,20 @@
+package chapter04;
+
+public class ObjectTest02 {
+
+	public static void main(String[] args) {
+		Point p1=new Point(10,20);
+		Point p2=new Point(10,20);
+		Point p3=p2;
+		
+		// == : 두 객체의 동일성 확인
+		System.out.println(p1==p2);//false
+		System.out.println(p2==p3);//true
+		
+		//equals : 두 객체의 동질성(내용비교)
+		//		 : 부모클래스 Object의 기본 구현은 동일성(==) ㅇ비교와 같다. 
+		System.out.println(p1.equals(p2)); //false
+		System.out.println(p2.equals(p3)); //true
+	}
+
+}
---
+++ chapter04/src/main/java/chapter04/Point.java
@@ -0,0 +1,36 @@
+package chapter04;
+
+
+public class Point{
+	private int x;
+	private int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public Point() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this==obj) return true;
+		if(obj==null) return false;
+		if(getClass()!=obj.getClass()) return false;
+		Point other = (Point) obj;
+		return (x==other.x && y==other.y);
+	}
+}
