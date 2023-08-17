ade1c86d1d98c7bb059fe5c990cc199089d71c1d
shin-6-0 <syk6715@naver.com>
2023-08-17 16:49:08 +0900

    NSLookup 프로그램

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,20 +1,33 @@
 package util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
-			System.out.println(">> ");
-			String line = sc.nextLine();
-			if("quit".equals(line)) {
-				break;
+		try {
+			boolean isTrue=true;
+			while(true) {
+				Scanner sc = new Scanner(System.in);
+				System.out.print(">> ");
+				String line = sc.nextLine();
+				if("quit".equals(line)) {
+					break;
+				}
+				InetAddress aArr[] = null;
+				aArr=InetAddress.getAllByName(line);
+				//System.out.println("arr 길이 : "+aArr.length);
+				for(int i=0;i<aArr.length;i++) {
+					System.out.println(aArr[i].getHostName()+" : "+aArr[i].getHostAddress());
+				}
 			}
+		}catch(UnknownHostException e) {
+			System.out.println("Error : "+e);
 		}
+		
 	}
 	//String host (도메인네임) 출력
 	
