48fa8cfc9d43bbf15a998c49a22bee51453db550
shin-6-0 <syk6715@naver.com>
2023-08-17 15:48:47 +0900

    InetAddress

---
+++ network/src/main/java/network/LocalHost.java
@@ -0,0 +1,29 @@
+package network;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class LocalHost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();//프로그램이 돌고 있는 것
+			
+			String hostname = inetAddress.getHostName();
+			String hostIpAddress = inetAddress.getHostAddress();
+			
+			System.out.println(hostname);
+			System.out.println(hostIpAddress);
+			
+			byte[] IpAddresses = inetAddress.getAddress();
+			for(byte IpAddress : IpAddresses) {
+				System.out.println(IpAddress&0x000000ff);//왜 0x000000ff를 곱해둔걸까
+			}
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ network/src/main/java/util/NSLookup.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.util.Scanner;
+
+public class NSLookup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println(">> ");
+			String line = sc.nextLine();
+			if("quit".equals(line)) {
+				break;
+			}
+		}
+	}
+	//String host (도메인네임) 출력
+	
+
+}
