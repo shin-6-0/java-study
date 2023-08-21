dd44d9908a1889cb8ad34f4f8b7d88705da37f6f
shin-6-0 <syk6715@naver.com>
2023-08-21 10:47:43 +0900

    httpd #finish

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -1,13 +1,17 @@
 package httpd;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENT_ROOT = "./webapp";
+	
 	private Socket socket;
 
 	public RequestHandler( Socket socket ) {
@@ -52,7 +56,9 @@ public class RequestHandler extends Thread {
 			}else {
 				//methods: POST, PUT, DELETE, + HEAD, CONNECT 도 있음
 				//SimpleHttpServer에서는 무시(400 Bad Request)
-				//responseStatic400Error(outputStream,tokens[2]);
+				
+				System.out.println("400 Bad Request:"+request);
+				responseStatic400Error(outputStream,tokens[2]);
 			}
 
 			// logging Remote Host IP Address & Port
@@ -62,11 +68,12 @@ public class RequestHandler extends Thread {
 
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-			outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
-			outputStream.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
-			outputStream.write( "\r\n".getBytes() );
-			outputStream.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
-
+			/*
+			 * outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
+			 * outputStream.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes(
+			 * "UTF-8" ) ); outputStream.write( "\r\n".getBytes() );
+			 outputStream.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
+			*/
 		} catch( Exception ex ) {
 			log( "error:" + ex );
 		} finally {
@@ -82,9 +89,46 @@ public class RequestHandler extends Thread {
 		}			
 	}
 
+	private void responseStatic400Error(OutputStream outputStream, String string) {
+		//HTTP/1.1 400 File Not Found \r\n
+		//Content-Type:text/html; charset=utf-8\r\n
+		//\r\n
+		//error/400.html 내용
+	}
+
 	private void responseStaticResource(
-			OutputStream outputStream, String url, String protocol) {
+			OutputStream outputStream, String url, String protocol) throws IOException {
+		//default(welcome) file
+		if("/".equals(url)) {
+			url="/index.html";
+		}
 		
+		File file =new File(DOCUMENT_ROOT+url);
+		if(!file.exists()) {
+			System.out.println("404 Not Found:"+url);
+			responseStatic404Error(outputStream,protocol);
+			return;
+		}
+		
+		// nio
+		byte[] body = Files.readAllBytes(file.toPath());
+		
+		String contentType = Files.probeContentType(file.toPath());
+		
+		// 응답
+		outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
+		outputStream.write( ("Content-Type:"+contentType+"; charset=utf-8\r\n").getBytes( "UTF-8" ) );
+		outputStream.write( "\r\n".getBytes() );
+		//outputStream.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
+		outputStream.write(body);
+	}
+
+	private void responseStatic404Error(
+			OutputStream outputStream, String protocol) {
+		//HTTP/1.1 404 File Not Found \r\n
+		//Content-Type:text/html; charset=utf-8\r\n
+		//\r\n
+		//error/400.html 내용
 	}
 
 	public void log( String message ) {
